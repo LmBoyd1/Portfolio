55ae2cb343a1382faf0fc0d3bdb9556805005348
Logan Boyd <loganboyd@Logans-MacBook-Pro.local>
2022-07-25 11:08:45 -0600

    fixed front end again

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navlinks = document.querySelectorAll('.nav__link')
+const navlinks = document.querySelectorAll('.nav__link');
 
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
 
-navlinks.forEach(link => {
+navlinks.forEach ('link', () => {
     document.body.classList.remove('.nav-open');
 }
